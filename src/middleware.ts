667501e048407e8c6afad4fab0d721d8bfcefc0d
Sergi <sgallegobastidas03@gmail.com>
2026-04-09 16:29:26 -0500

    feat(billboard): create Billboard component with basic structure

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req });
     const pathname = req.nextUrl.pathname;
