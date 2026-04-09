13e1d6555048c79d74b6a8a65a1a5a512e9049fc
Sergi <sgallegobastidas03@gmail.com>
2026-04-09 16:43:11 -0500

    feat(billboard): enhance Billboard component with movie fetching logic and state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Billboard from "@/shared/components/Billboard";
 import Navbar from "@/shared/components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Billboard />
     </div>
    );
 }
--- src/shared/components/Billboard.tsx
+++ src/shared/components/Billboard.tsx
@@ -1,5 +1,28 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { IMovie } from "@/types/movie.types";
+
 const Billboard = () => {
-    return  <div>Billboard</div>;
-};
+    const [randomMovie, setRandomMovie] = useState<IMovie | null>(null);
+
+    const fetchMovies = async () => {
+        try {
+            const { data } = await axios.get("/api/movies");
+            const randomNum = Math.floor(Math.random() * data.length);
+            setRandomMovie(data[randomNum]);
+        } catch (error) {
+            console.log(error);
+        } // <- Se cierra el catch
+    }; // <- Se cierra la función fetchMovies AQUÍ
+
+    useEffect(() => {
+        fetchMovies();
+    }, []); // <- El useEffect ahora está en el nivel principal del componente
+
+    console.log("Random Movie: ", randomMovie);
+
+    return <div className="h-screen relative">Billboard</div>;
+}; // <- Se cierra el componente Billboard
 
 export default Billboard;
