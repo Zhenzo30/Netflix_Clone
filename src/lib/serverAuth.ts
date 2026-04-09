9601f1bc62120f1b6575c51ae2f0698f91cbd9e5
Sergi <sgallegobastidas03@gmail.com>
2026-04-08 20:30:26 -0500

    feat(auth): implement serverAuth function for user session validation

---
+++ src/lib/serverAuth.ts
@@ -0,0 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/option";
+import User from "@/models/User";
+import { getServerSession } from "next-auth/next";
+
+const serverAuth = async () => {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            throw new Error("Unauthorized");
+        }
+
+        const currentUser = await User.findOne({ email: session.user?.email });
+        if (!currentUser) {
+            throw new Error("Unauthorized");
+        }
+
+        return currentUser;
+    } catch {
+        throw new Error("Unauthorized");
+    }
+};
+
+export default serverAuth;
