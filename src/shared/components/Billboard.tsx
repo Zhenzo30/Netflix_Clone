bd2ab23537569896dde6d98e83dd94b3a8ffcb6a
Sergi <sgallegobastidas03@gmail.com>
2026-04-10 17:15:58 -0500

    feat(billboard): enhance Billboard component with movie details and play button functionality

--- src/shared/components/Billboard.tsx
+++ src/shared/components/Billboard.tsx
@@ -3,13 +3,14 @@ import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react"; // <-- Importamos useSession
 import { IMovie } from "@/types/movie.types";
+import Image from "next/image";
 
 const Billboard = () => {
     const [randomMovie, setRandomMovie] = useState<IMovie | null>(null);
     const videoRef = useRef<HTMLVideoElement | null>(null);
     
     // <-- El semáforo
-    const { data: session, status } = useSession();
+    const { status } = useSession();
 
     const fetchMovies = async () => {
         try {
@@ -22,6 +23,13 @@ const Billboard = () => {
             console.error("Error fetching movies:", error);
         }
     };
+
+    const handlePlayButtonClick = () => {
+        if (videoRef.current) {
+            videoRef.current.requestFullscreen();
+        }
+    };
+
 
     useEffect(() => {
         // Solo disparamos la petición SI el estado es "authenticated" (Ya está logueado)
@@ -42,24 +50,46 @@ const Billboard = () => {
     }
 
     return (
-        <div className="h-screen relative bg-black">
-            {randomMovie ? (
-                <video 
-                    src={randomMovie.videoUrl} 
-                    poster={randomMovie.thumbnailUrl}
-                    ref={videoRef}
-                    className="w-full h-full object-cover"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                />
-            ) : (
-                <div className="w-full h-full flex items-center justify-center">
-                    <p className="text-white text-2xl animate-pulse">Cargando película...</p>
+
+        <div>
+            <video 
+                src={randomMovie?.videoUrl}
+                poster={randomMovie?.thumbnailUrl}
+                ref={videoRef}
+                className="w-full h-full object-cover"
+                autoPlay
+                muted
+                loop
+                playsInline
+            />
+            <div
+                className="absolute top-1/2 left-10 -translate-y-1/2 transform flex flex-col gap-4"
+            >
+                <h1 className="text-5xl text-white font-bold">{randomMovie?.title}</h1>
+                <p className="text-white text-lg">{randomMovie?.description}</p>
+                <div className="flex gap-2">
+                    <button className="text-lg font-semibold bg-white py-2 px-5 text-black rounded-sm cursor-pointer flex gap-4 hover:bg-[#ffffffbf]" onClick={handlePlayButtonClick}>
+                        <Image
+                            src="/assets/play.svg"
+                            width={24}
+                            height={24}
+                            alt="Play video"
+                        />
+                        Play
+                    </button>
+                    <button className="text-lg font-semibold bg-[#6d6d6eb3] py-2 px-5 text-white rounded-sm cursor-pointer flex gap-4 hover:bg-[#6d6d6e66]" >
+                        <Image
+                            src="/assets/info.svg"
+                            width={24}
+                            height={24}
+                            alt="Play video"
+                        />
+                        More info
+                    </button>
                 </div>
-            )}
+            </div>
         </div>
+        
     );
 }; 
 
