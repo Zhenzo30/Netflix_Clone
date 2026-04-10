43f51e0a6816b33461b71ce8764abed08c4793a3
Sergi <sgallegobastidas03@gmail.com>
2026-04-10 18:53:09 -0500

    feat(MovieList): implement movie fetching and display in MovieList component

--- src/shared/components/MovieList.tsx
+++ src/shared/components/MovieList.tsx
@@ -1,5 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { IMovie } from "@/types/movie.types";
+
+
 const MovieList = () => {  
-    return <div>MovieList</div>
+
+    const [movies, setMovies] = useState<IMovie[]>([]);
+
+    const fetchMovies = async () => {
+        try {
+            const { data } = await axios.get("/api/movies");
+            setMovies(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    return <Movies movies={movies} label="Top Movies"/>;
 };
 
 export default MovieList;
