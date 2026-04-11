7fcfe5f92ec4c0a8fad087ecf42bb1e0b3240bc3
Sergi <sgallegobastidas03@gmail.com>
2026-04-10 19:45:31 -0500

    feat(MoviePopup): update MoviePopup to accept movie prop and render video with thumbnail

--- src/shared/components/MoviePopup.tsx
+++ src/shared/components/MoviePopup.tsx
@@ -1,14 +1,19 @@
+"use client";
+import { IMovie } from "@/types/movie.types";
 import { useRef } from "react";
 
-const MoviePopup = () => {
-
-const videoRef = useRef<HTMLVideoElement | null>(null);
+const MoviePopup = ({ movie }: { movie: IMovie }) => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
 
     return (
-        <div className="absolute -left-8 w-80 z-10 -top-16 rounded-md text-base transform sacle-75 hover:scale-100 hover:-translate-y-6 transition-transform duration-300 ease-in-out">
-            <video />
+        <div className="absolute -left-8 w-80 z-10 -top-16 rounded-md text-base transform scale-75 hover:scale-100 hover:-translate-y-6 transition-transform duration-300 ease-in-out">
+            <video 
+                ref={videoRef}
+                src={movie.videoUrl}
+                poster={movie.thumbnailUrl}
+            />
         </div>
     );
-}
+};
 
 export default MoviePopup;
