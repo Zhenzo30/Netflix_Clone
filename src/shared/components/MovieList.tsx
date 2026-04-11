7634714fb03fe00e188a53a310231908ddd9fc32
Sergi <sgallegobastidas03@gmail.com>
2026-04-10 19:36:22 -0500

    feat(Movies): add image rendering for movie thumbnails and update Movies component structure feat(MovieList): enable client-side rendering for MovieList component feat(next.config): enable unoptimized images for external sources

--- src/shared/components/MovieList.tsx
+++ src/shared/components/MovieList.tsx
@@ -1,3 +1,4 @@
+"use client";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { IMovie } from "@/types/movie.types";
--- src/shared/components/Movies.tsx
+++ src/shared/components/Movies.tsx
@@ -1,7 +1,27 @@
+"use client";
 import { IMovie } from "@/types/movie.types";
+import Image from "next/image";
 
 const Movies = ({ movies, label }: { movies: IMovie[]; label: string }) => {
-    return <div>Movies</div>
+    return (
+        <div className="flex flex-col gap-2 relative my-[3vw] px-[4%]">
+            {movies.length ?  <h2 className="text-[#e5e5e5] font-medium text-xl">{label}</h2> : null}
+            <div className="flex gap-2">
+                {movies.map((movie) => (
+                    <div key={movie._id} className="relative cursor-pointer">
+                        <div className="relative cursor-pointer w-[250px] h-[140px] rounded-sm transition duration-300 ease-in-out">
+                            <Image
+                                src={movie.thumbnailUrl}
+                                className="object-cover"
+                                alt={movie.title}
+                                fill
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 };
 
 export default Movies;
