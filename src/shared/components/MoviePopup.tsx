3427641edcc0fa6b99323328e1cbf751ea4c6f7f
Sergi <sgallegobastidas03@gmail.com>
2026-04-10 21:45:53 -0500

    feat(MoviePopup, Movies): enhance MoviePopup with video playback and integrate into Movies component

--- src/shared/components/MoviePopup.tsx
+++ src/shared/components/MoviePopup.tsx
@@ -1,9 +1,34 @@
 "use client";
 import { IMovie } from "@/types/movie.types";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { clsx } from "clsx";
 
-const MoviePopup = ({ movie }: { movie: IMovie }) => {
+const MoviePopup = ({ movie, handleOpenInfoModal }: { movie: IMovie; handleOpenInfoModal: () => void }) => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+    const handlePlayButtonClick = () => {
+        if (videoRef.current) {
+            setIsVideoPlaying(true);
+            videoRef.current.play();
+            videoRef.current.requestFullscreen();
+        }
+    };
+
+    useEffect(() => {
+        const handleFullscreenChange = () => {
+            if (!document.fullscreenElement) {
+                setIsVideoPlaying(false);
+            }
+        };
+
+        document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+        return () => {
+            document.removeEventListener("fullscreenchange", handleFullscreenChange);
+        };
+    }, []);
 
     return (
         <div className="absolute -left-8 w-80 z-10 -top-16 rounded-md text-base transform scale-75 hover:scale-100 hover:-translate-y-6 transition-transform duration-300 ease-in-out">
@@ -11,7 +36,56 @@ const MoviePopup = ({ movie }: { movie: IMovie }) => {
                 ref={videoRef}
                 src={movie.videoUrl}
                 poster={movie.thumbnailUrl}
+                className={clsx({ hidden: !isVideoPlaying })}
             />
+            <div className="relative w-80 h-[140px]">
+                <Image
+                    src={movie.thumbnailUrl}
+                    alt={movie.title}
+                    fill
+                    className="object-cover"
+                />
+            </div>
+            <div className="bg-[#181818] p-4">
+                <div className="flex justify-between mb-2 items-center">
+                    <div className="flex gap-2 items-center">
+                        <button 
+                            className="bg-white border-2 rounded-full p-2 border-[#ffffff80] cursor-pointer"
+                            onClick={handlePlayButtonClick}
+                        >
+                            <Image src="/assets/play.svg" alt="Play" width={20} height={20} />
+                        </button>
+                        
+                        {/* ---> FIX AQUÍ: Se quitó el handlePlayButtonClick y se dejó vacío () => {} <--- */}
+                        <button 
+                            className="bg-[#2a2a2a99] border-2 rounded-full p-2 border-[#ffffff80] cursor-pointer"
+                            onClick={() => {}} // No hace nada por ahora
+                        >
+                            <Image src="/assets/plus.svg" alt="Add" width={20} height={20} />
+                        </button>
+                    </div>
+                    
+                    <button 
+                        className="cursor-pointer bg-[#2a2a2a99] border-2 border-[#ffffff80] p-2 rounded-full hover:bg-[#ffffff1a] hover:border-white" 
+                        onClick={handleOpenInfoModal}
+                    >
+                        <Image 
+                            src="/assets/down-arrow.svg"
+                            width={24}
+                            height={24}
+                            alt="Show movie details"
+                        />
+                    </button>
+                </div>
+                
+                <div className="flex gap-2 items-center mt-4">
+                    <span className="px-2 uppercase whitespace-nowrap text-[#bcbcbc] text-sm font-medium border border-[#fff6]">U/A 13+</span>
+                    <span className="text-[#bcbcbc] text-base">{movie.duration}</span>
+                    <span className="text-[#bcbcbc] border border-[#fff6] rounded-[3px] text-xs px-1.5">HD</span>
+                </div>
+                
+                <p className="textShadow text-base text-white mt-3">{movie.genre}</p>
+            </div>
         </div>
     );
 };
--- src/shared/components/Movies.tsx
+++ src/shared/components/Movies.tsx
@@ -1,27 +1,65 @@
 "use client";
 import { IMovie } from "@/types/movie.types";
 import Image from "next/image";
+import { useState } from "react";
+import MoviePopup from "./MoviePopup";
+import MovieInfoModal from "./MovieInfoModal";
 
 const Movies = ({ movies, label }: { movies: IMovie[]; label: string }) => {
+    const [movieData, setMovieData] = useState<IMovie | null>(null);
+    const [movieId, setMovieId] = useState<string | null>(null);
+    const [showInfoModal, setShowInfoModal] = useState(false);
+
+    const handleMouseEnter = (movie: IMovie, movieId: string) => {
+        setMovieData(movie);
+        setMovieId(movieId);
+    };
+
+    const handleMouseLeave = () => {
+        setMovieId(null);
+    };
+
+    const handleOpenInfoModal = () => {
+        setMovieId(null);
+        setShowInfoModal(true);
+    };
+
     return (
-        <div className="flex flex-col gap-2 relative my-[3vw] px-[4%]">
-            {movies.length ?  <h2 className="text-[#e5e5e5] font-medium text-xl">{label}</h2> : null}
-            <div className="flex gap-2">
-                {movies.map((movie) => (
-                    <div key={movie._id} className="relative cursor-pointer">
-                        <div className="relative cursor-pointer w-[250px] h-[140px] rounded-sm transition duration-300 ease-in-out">
-                            <Image
-                                src={movie.thumbnailUrl}
-                                className="object-cover"
-                                alt={movie.title}
-                                fill
-                            />
+        <>
+            <div className="flex flex-col gap-2 relative my-[3vw] px-[4%]">
+                {movies.length ? <h2 className="text-[#e5e5e5] font-medium text-xl">{label}</h2> : null}
+                <div className="flex gap-2">
+                    {movies.map((movie) => (
+                        <div 
+                            key={movie._id} 
+                            className="relative cursor-pointer" 
+                            onMouseEnter={() => handleMouseEnter(movie, movie._id)} 
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <div className="relative cursor-pointer w-[250px] h-[140px] rounded-sm transition duration-300 ease-in-out">
+                                <Image
+                                    src={movie.thumbnailUrl}
+                                    className="object-cover"
+                                    alt={movie.title}
+                                    fill
+                                />
+                            </div>
+                            {movieId === movie._id ? <MoviePopup movie={movie} handleOpenInfoModal={handleOpenInfoModal} /> : null}
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+
+            {showInfoModal ? (
+                // ---> FIX AQUÍ: Se quitó el espacio después del < <---
+                <MovieInfoModal 
+                    showInfoModal={showInfoModal} 
+                    setShowInfoModal={setShowInfoModal} 
+                    movieData={movieData} 
+                /> 
+            ) : null}
+        </>
+    );
 };
 
 export default Movies;
